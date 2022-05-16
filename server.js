931dcfc85c7a4f2869f8bcf9029d6f8249d427ae
David VanDusen <github@dvw3.com>
2022-05-16 11:29:31 -0700

    Add idempotent schema.sql and run on server startup

---
+++ server.js
@@ -0,0 +1,13 @@
+const main = require('./src/main');
+
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT;
+const pgConfig = {
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+};
+
+main(hostname, port, pgConfig);
