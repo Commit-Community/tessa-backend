d6078bbde77a1eb1f7c83af2690644feb0e30196
David VanDusen <github@dvw3.com>
2022-05-19 11:27:23 -0700

    Add route for admins to create facets

--- src/__tests__/facetsRouter.js
+++ src/__tests__/facetsRouter.js
@@ -1,9 +1,11 @@
-const { collectionEnvelope } = require("../responseEnvelopes");
+const { collectionEnvelope, itemEnvelope } = require("../responseEnvelopes");
 const { createAppAgentForRouter } = require("../routerTestUtils");
 const facetsRouter = require("../facetsRouter");
-const { listFacets } = require("../facetsService");
+const { isAdmin } = require("../authMiddleware");
+const { listFacets, createFacet } = require("../facetsService");
 
 jest.mock("../facetsService");
+jest.mock("../authMiddleware");
 
 describe("facetsRouter", () => {
   const appAgent = createAppAgentForRouter(facetsRouter);
@@ -15,6 +17,7 @@ describe("facetsRouter", () => {
           id: 1,
           name: "test name",
           recommendation_prompt: "test recommendation prompt",
+          sort_order: 0,
         },
       ];
       listFacets.mockResolvedValueOnce(facets);
@@ -22,6 +25,108 @@ describe("facetsRouter", () => {
         expect(listFacets).toHaveBeenCalled();
         done(err);
       });
+    });
+  });
+
+  describe("POST /", () => {
+    beforeEach(() => {
+      isAdmin.mockImplementation((req, res, next) => next());
+    });
+
+    it("should create a facet with the given data", (done) => {
+      const name = "test name";
+      const recommendationPrompt = "test recommendation prompt";
+      const sortOrder = 0;
+      const facet = {
+        id: 1,
+        name,
+        recommendation_prompt: recommendationPrompt,
+        sort_order: sortOrder,
+      };
+      createFacet.mockResolvedValueOnce(facet);
+      appAgent
+        .post("/")
+        .send({
+          name: name,
+          recommendation_prompt: recommendationPrompt,
+          sort_order: sortOrder,
+        })
+        .expect(201, itemEnvelope(facet), (err) => {
+          expect(createFacet).toHaveBeenCalledWith(
+            name,
+            recommendationPrompt,
+            sortOrder
+          );
+          done(err);
+        });
+    });
+
+    it("should respond with an unprocessable entity error if name is not sent", (done) => {
+      appAgent
+        .post("/")
+        .send({
+          recommendation_prompt: "test recommendation prompt",
+          sort_order: 0,
+        })
+        .expect(422, done);
+    });
+
+    it("should respond with an unprocessable entity error if name is not valid", (done) => {
+      appAgent
+        .post("/")
+        .send({
+          name: "",
+          recommendation_prompt: "test recommendation prompt",
+          sort_order: 0,
+        })
+        .expect(422, done);
+    });
+
+    it("should respond with an unprocessable entity error if recommendation_prompt is not sent", (done) => {
+      appAgent
+        .post("/")
+        .send({ name: "test name", sort_order: 0 })
+        .expect(422, done);
+    });
+
+    it("should respond with an unprocessable entity error if recommendation_prompt is not valid", (done) => {
+      appAgent
+        .post("/")
+        .send({ name: "test name", recommendation_prompt: "", sort_order: 0 })
+        .expect(422, done);
+    });
+
+    it("should respond with an unprocessable entity error if sort_order is not sent", (done) => {
+      appAgent
+        .post("/")
+        .send({
+          name: "test name",
+          recommendation_prompt: "test recommendation prompt",
+        })
+        .expect(422, done);
+    });
+
+    it("should respond with an unprocessable entity error if sort_order is not valid", (done) => {
+      appAgent
+        .post("/")
+        .send({
+          name: "test name",
+          recommendation_prompt: "test recommendation prompt",
+          sort_order: "bad value",
+        })
+        .expect(422, done);
+    });
+
+    it("should respond with an internal server error if the facet couldn't be created", (done) => {
+      createFacet.mockRejectedValueOnce(new Error());
+      appAgent
+        .post("/")
+        .send({
+          name: "test name",
+          recommendation_prompt: "test recommendation prompt",
+          sort_order: 0,
+        })
+        .expect(500, done);
     });
   });
 });
--- src/facetsRouter.js
+++ src/facetsRouter.js
@@ -1,13 +1,61 @@
 const { Router } = require("express");
 
-const { collectionEnvelope } = require("./responseEnvelopes");
-const { listFacets } = require("./facetsService");
+const { collectionEnvelope, itemEnvelope } = require("./responseEnvelopes");
+const { isAdmin } = require("./authMiddleware");
+const { isNonWhitespaceOnlyString } = require("./validators");
+const { listFacets, createFacet } = require("./facetsService");
+const { UnprocessableEntityError } = require("./httpErrors");
 
 const facetsRouter = new Router();
 
 facetsRouter.get("/", async (req, res) => {
   const facets = await listFacets();
   res.json(collectionEnvelope(facets, facets.length));
+});
+
+facetsRouter.post("/", isAdmin, async (req, res, next) => {
+  if (
+    typeof req.body !== "object" ||
+    !("name" in req.body) ||
+    !("recommendation_prompt" in req.body) ||
+    !("sort_order" in req.body)
+  ) {
+    next(
+      new UnprocessableEntityError(
+        "The request body must be an object with name, recommendation_prompt and sort_order properties."
+      )
+    );
+    return;
+  }
+  const {
+    name,
+    recommendation_prompt: recommendationPrompt,
+    sort_order: sortOrder,
+  } = req.body;
+  if (!isNonWhitespaceOnlyString(name)) {
+    next(new UnprocessableEntityError(`"${name}" must contain text.`));
+    return;
+  }
+  if (!isNonWhitespaceOnlyString(recommendationPrompt)) {
+    next(
+      new UnprocessableEntityError(
+        `"${recommendationPrompt}" must contain text.`
+      )
+    );
+    return;
+  }
+  if (!Number.isSafeInteger(sortOrder)) {
+    next(new UnprocessableEntityError(`"${sortOrder}" must be an integer.`));
+    return;
+  }
+  let facet;
+  try {
+    facet = await createFacet(name, recommendationPrompt, sortOrder);
+  } catch (e) {
+    next(e);
+    return;
+  }
+  res.status(201).json(itemEnvelope(facet));
 });
 
 module.exports = facetsRouter;
